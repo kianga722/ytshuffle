ab433e619d3bf102e92205eee34a9214a3fccc6f
Top Tester <json900064@gmail.com>
2018-09-10 21:50:06 -0400

    Trying to trigger from video end"

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,34 @@
 class StaticPagesController < ApplicationController
   
   def home
+  end
+
+  def shuffle
     #channel = Yt::Channel.new id: 'UC4rasfm9J-X4jNl9SvXp8xA'
     
-    #playlist = Yt::Playlist.new id: 'UU4rasfm9J-X4jNl9SvXp8xA'
-    #@title = playlist.title
-    #@list = playlist.playlist_items
-    #@count = @list.count
-    #@goodlist = @list.take(@count)
+    #Only create playlist the first time
+    @list = JSON.load(params[:list])
 
-    #@rand_vid = @goodlist.sample
+    unless @list
+      @goodlist = []
+      @playlist = Yt::Playlist.new id: "#{params[:list_id]}"
+      @title = @playlist.title
+      @list = @playlist.playlist_items
+      @count = @list.count
 
-    #@video = Yt::Video.new id: "#{@rand_vid.video_id}"
-    @video = Yt::Video.new id: "TOKHdJO_u2A"
+      #Make array of video arrays
+      @list.take(@count).each do |i|
+        @goodlist << [i.title, i.position, i.video_id]  
+      end
+    else
+      @goodlist = JSON.load(params[:goodlist])
+      @title = params[:title]
+      @count = params[:count]
+    end
 
+    @rand_vid = @goodlist.sample
+
+    @video = Yt::Video.new id: "#{@rand_vid[2]}"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/shuffle', to: 'static_pages#shuffle'
+  post '/shuffle', to: 'static_pages#shuffle'
 end
