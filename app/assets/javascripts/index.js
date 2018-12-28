92c7ad8671fe57f70f6fc1a596b0be6892f9b15f
Top Tester <json900064@gmail.com>
2018-12-28 00:40:22 -0500

    Issue with chrome error handling

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,37 +1,3 @@
-//YT iFrame API
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('existing-iframe-example', {
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-            'onError': onPlayerError
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-    if(event.data === 0) {
-        let nextSong = document.getElementById('next');
-        Rails.fire(nextSong, 'submit');
-    }
-}
-
-function onPlayerError() {
-    let nextSong = document.getElementById('next');
-    Rails.fire(nextSong, 'submit');
-}
-
 //Loading Logic Module
 const loadLogic = (() => {
   //Function to hide/unhide elements
@@ -54,8 +20,13 @@ const loadLogic = (() => {
 })();
 
 document.addEventListener('turbolinks:load', function() {
-    document.body.addEventListener('submit', () => {
-        loadLogic.showLoad(true);
+    const formArr = ['any', 'thrilling', 'proxmity', 'ultra', 'next'];
+
+    document.body.addEventListener('click', e => {
+        let target = e.target;
+        if (formArr.includes(target.parentNode.id)) {
+            loadLogic.showLoad(true);
+        }
     })
 })
 
